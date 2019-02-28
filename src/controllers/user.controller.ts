a8223073ccb6bae04f24f7df9a5b25e59095c501
Marko Koskinen <markokoskinen2037@gmail.com>
2019-02-28 12:28:41 +0200

    add user works

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from 'express'
 
 const router: Router = Router()
 
+import database from '../database'
+
 // Get all users
 router.get('/', (req: Request, res: Response) => {
   res.send('Create user with POST')
@@ -13,9 +15,15 @@ router.get('/:id', (req: Request, res: Response) => {
 })
 
 // Create a user
-router.post('/', (req: Request, res: Response) => {
-  console.log(req.body)
-  res.json({ message: 'todo' })
+router.post('/', async (req: Request, res: Response) => {
+  const name = req.body.name
+  const googleid = req.body.googleid
+
+  const id = await database('users').insert({ name, googleid }, 'id')
+
+  const user = await database('users').select('*')
+
+  res.json({ user })
 })
 
 export const LoginController: Router = router
