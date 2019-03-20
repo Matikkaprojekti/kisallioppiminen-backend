dd725c90d3f855ba005b0d93af98952451539d27
Marko Koskinen <markokoskinen2037@gmail.com>
2019-03-20 12:41:56 +0200

    implement join/coursekey route

--- src/controllers/teachinginstance.controller.ts
+++ src/controllers/teachinginstance.controller.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express'
 import passport from 'passport'
-import { findOrCreate } from '../services/teachingInstanceService'
+import { findOrCreateTeachinginstance, findTeachinginstanceByCoursekey } from '../services/teachingInstanceService'
+import { findUserById } from '../services/userService'
+import { findOrCreateUsersTeachinginstance } from '../services/usersTeachingInstancesService'
 
 const router: Router = Router()
 
@@ -10,9 +12,44 @@ router.post('/', (req: Request, res: Response) => {
 
   // Check that required params are present
   if (coursekey && coursematerial_name && coursematerial_version && name && startdate && enddate) {
-    const result = findOrCreate(req.body).then(r => res.json(r))
+    const result = findOrCreateTeachinginstance(req.body).then(r => res.json(r))
   } else {
-    res.json({ error: 'Some requestparams are missing...' })
+    res.status(400)
+    res.json({ error: 'Bad request' })
+  }
+})
+
+// Student join a teachinginstance with the key of the instance.
+router.post('/join/:coursekey', async (req: Request, res: Response) => {
+  const coursekey = req.params.coursekey
+  const { user_id, teacher } = req.body
+
+  console.log(coursekey, user_id, teacher)
+
+  if (coursekey && user_id && teacher !== undefined) {
+    console.log('Required params are present.')
+    console.log('Checking if coursekey and user_id exists...')
+
+    const user = await findUserById(user_id)
+    const teachinginstance = await findTeachinginstanceByCoursekey(coursekey)
+
+    console.log('user ', user)
+    console.log('teachinginstance', teachinginstance)
+    if (user && teachinginstance) {
+      console.log('Lisätään käyttäjä opetusinstanssiin...')
+
+      const newInstances = { user_id, course_coursekey: coursekey, teacher }
+
+      const result = await findOrCreateUsersTeachinginstance(newInstances)
+
+      res.json({ teachinginstance: result })
+    } else {
+      res.status(400)
+      res.send('User or Teachinginstance not found!')
+    }
+  } else {
+    res.status(400)
+    res.send('Very BAD request.')
   }
 })
 
